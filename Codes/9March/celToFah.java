813e4176330d1ff6a6c0eabaccb90f9b5be7b8f3
Khagesh Nemade <128049399+khageshnemade@users.noreply.github.com>
2023-03-18 09:52:41 +0530

    Delete Codes directory

--- Codes/9March/celToFah.java
+++
@@ -1,8 +0,0 @@
-class celToFah{
- static  final public void main(String[]args  )      {
-int cel= 30;
-System.out.println("The value of temperature in celcius is "+cel);
-System.out.println("the vaaluein fahren is "+((cel * 9/5) + 32));
-
-}
-}
